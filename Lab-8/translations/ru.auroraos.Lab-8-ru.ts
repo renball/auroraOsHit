8c2df6a56b386cd2420da6a22045e2fcae2e741a
YablonskiiDmitriy <72209562+renball@users.noreply.github.com>
2024-01-30 06:51:28 +0300

    Add files via upload

---
+++ Lab-8/translations/ru.auroraos.Lab-8-ru.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="46"/>
+        <source>Lab-8</source>
+        <translation>Шаблон</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="55"/>
+        <source>Лабораторная номер 8</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>task3</name>
+    <message>
+        <location filename="../qml/pages/task3.qml" line="8"/>
+        <source>Задание 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>task4</name>
+    <message>
+        <location filename="../qml/pages/task4.qml" line="9"/>
+        <source>Задание 4</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>task5</name>
+    <message>
+        <location filename="../qml/pages/task5.qml" line="9"/>
+        <source>Задание 5</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>task6</name>
+    <message>
+        <location filename="../qml/pages/task6.qml" line="9"/>
+        <source>Задание 6</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>task7</name>
+    <message>
+        <location filename="../qml/pages/task7.qml" line="9"/>
+        <source>Задание 7</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>task8</name>
+    <message>
+        <location filename="../qml/pages/task8.qml" line="9"/>
+        <source>Задание 8</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
